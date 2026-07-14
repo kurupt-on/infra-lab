ff0bde07d01abae4ace61060c9d43fc02d02a42e
kurupt <kurupt_on@proton.me>
2026-07-14 19:04:29 -0300

    chore: Adiciona host para testes

---
+++ terraform/proxmox/containers/test.tf
@@ -0,0 +1,55 @@
+resource "proxmox_virtual_environment_container" "test" {
+  node_name    = var.node_name
+  vm_id        = "110"
+  unprivileged = true
+  started      = true
+
+  features {
+    nesting = true
+  }
+
+  initialization {
+    hostname = "test"
+
+  user_account {
+    keys  = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEh0Il2n62X4e45MRKVcdqLfHRakBhJm5ZSSjc+6Jg9c ansible@corp.local"]
+  }
+
+    ip_config {
+      ipv4 {
+        address = "10.0.0.10/24"
+        gateway = "10.0.0.1"
+      }
+    }
+  }
+
+  network_interface {
+    name   = "eth0"
+    bridge = "vmbr1"
+  }
+
+  network_interface {
+    name   = "eth1"
+    bridge = "vmbr0"
+  }
+
+  operating_system {
+    template_file_id  = var.template
+    type              = "debian" 
+  }
+
+  cpu {
+    cores = 1
+  }
+
+  memory {
+    dedicated = 512
+    swap      = 512
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 8
+  }
+}
+
